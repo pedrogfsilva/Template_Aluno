0b8f8959634657790e0bf3a482fcd0511de9c8fb
pedrogfsilva <pedro.silva@sou.inteli.edu.br>
2022-05-28 12:31:23 -0300

    Retornando informações do banco de dados, exercício etapa 5

--- 02_TUTORIAL/Semana 1/app_15.js
+++ 02_TUTORIAL/Semana 1/app_15.js
@@ -1,10 +1,25 @@
 const express = require('express'); 
-const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
+const app = express();
 const hostname = '127.0.0.1';
 const port = 3015;
 
+const DBPATH = './database/curriculo.db';
+const db = new sqlite3.Database(DBPATH);
+
 app.use(express.static("public"));
+
+app.get('/curriculo', (req, res) =>{
+  const sql = 'SELECT * FROM curriculoPedro';
+  db.all(sql, [], (err, rows) =>{
+    if(err){
+      throw err;
+    } else {
+      res.json(rows);
+    }
+  });
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server is running at http://${hostname}:${port}/`);
