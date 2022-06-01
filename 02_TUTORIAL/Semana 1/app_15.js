3b2afac8e2c46ee913736e3b10630bbc4f1a5e46
pedrogfsilva <pedro.silva@sou.inteli.edu.br>
2022-06-01 12:07:17 -0300

    crud curriculo

--- 02_TUTORIAL/Semana 1/app_15.js
+++ 02_TUTORIAL/Semana 1/app_15.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 
 const app = express();
 const hostname = '127.0.0.1';
@@ -9,6 +10,8 @@ const DBPATH = './database/curriculo.db';
 const db = new sqlite3.Database(DBPATH);
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/curriculo', (req, res) =>{
   const sql = 'SELECT * FROM curriculoPedro';
@@ -17,6 +20,53 @@ app.get('/curriculo', (req, res) =>{
       throw err;
     } else {
       res.json(rows);
+    }
+  });
+});
+
+app.post('/curriculo', (req, res) =>{
+  const firstName = req.body.nome;
+  const lastName = req.body.sobrenome;
+  const age = req.body.idade;
+  const education = req.body.formacao;
+
+  const sql = 'INSERT INTO curriculoPedro (nome, sobrenome, idade, formacao) VALUES (?, ?, ?, ?)';
+  db.run(sql, [firstName, lastName, age, education], (err) =>{
+    if(err){
+      throw err;
+    } else {
+      res.send('Cadastrado com sucesso!');      
+    }
+  });
+});
+
+app.patch('/curriculo/:id', (req, res) =>{
+  const { id } = req.params; 
+
+  const firstName = req.body.nome;
+  const lastName = req.body.sobrenome;
+  const age = req.body.idade;
+  const education = req.body.formacao;
+
+  const sql = `UPDATE curriculoPedro SET nome = ?, sobrenome = ?, idade = ?, formacao = ? WHERE id = ${id}`;
+  db.run(sql, [firstName, lastName, age, education], (err) =>{
+    if(err){
+      throw err;
+    } else {
+      res.send('Atualizado com sucesso!');
+    }
+  });
+});
+
+app.delete('/curriculo/:id', (req, res) =>{
+  const { id } = req.params;
+
+  const sql = `DELETE FROM curriculoPedro WHERE id = ${id}`;
+  db.run(sql, [], (err) =>{
+    if(err){
+      throw err;
+    } else {
+      res.send('Deletado com sucesso!');
     }
   });
 });
